a59b802c43e30f7589f5c7c51a6e1a373f995e5c
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-09-29 07:14:42 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[0]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
+
+scanner.addListener('scan', function (content) {
+  console.log(content);
+});
