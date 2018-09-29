15ddb090651628d0c11128b8f0c3e62e2dea00d5
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-09-29 08:24:12 +0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let scanner = new Instascan.Scanner({ video: document.getElementById('page__video') });
+const wrapper = document.getElementById('page__video')
+const card = document.querySelector('.card-body')
+const scanner = new Instascan.Scanner({ video: wrapper });
 
 Instascan.Camera.getCameras().then(function (cameras) {
   if (cameras.length > 0) {
@@ -11,5 +13,11 @@ Instascan.Camera.getCameras().then(function (cameras) {
 });
 
 scanner.addListener('scan', function (content) {
-  console.log(content);
+  const data = JSON.parse(content)
+  wrapper.style.display = 'none'
+  card.classList.remove('scanning')
+  card.innerHTML = '' +
+    '<div class="page__name">Welcome, '+ data.name +'.</div>' + 
+    '<div class="page__date">Attend on : '+ data.date +'</div>'
+    '<div class="page__location">Your Position : '+ data.location +'</div>'
 });
